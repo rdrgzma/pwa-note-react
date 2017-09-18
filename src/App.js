28589ab594476b948dff52b3b8a06ca714545c07
Marcio Alexandre Rodriguez de Rodrigues <marcio@debian>
2017-09-17 22:11:17 -0300

    Add styles css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return <div className="grey lighten-5 blue-grey-text">
         <div id="header" className="container grey lighten-4">
-          <h1>PWA</h1>
+          <h1>PWA NOTES</h1>
         </div>
         <div className="container grey lighten-4">
           <p>Adicione suas anotações aqui!</p>
